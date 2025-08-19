c2b809915ef17c4f16db528dc5e0e0554868cf53
alrodsa <alrodsa2@inf.upv.es>
2025-08-19 16:56:39 +0000

    feat: Implement 'douga' command in CLI

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,9 +1,11 @@
 import fire
 
+from src.cli.douga import douga
+
 def main() -> None:
     """
     CLI entrypoint.
     """
     fire.Fire({
-        "douga": "douga",
+        "douga": douga,
     })
