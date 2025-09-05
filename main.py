de24ce6d44d91beec914ccf77d8b51487ce880d9
alrodsa <alrodsa2@inf.upv.es>
2025-09-05 11:43:08 +0000

    feat: enhance logging in douga command for better traceability and information output

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from src.constants.app import APP_NAME
 from src.utils.pprint import banner
 from src.cli.main import main
+from saibyo.base.logs.logger import configure_logging
 
 if __name__ == "__main__":
+    configure_logging(APP_NAME, root_dir=".", logs_folder="logs")
     print(banner())
     main()
